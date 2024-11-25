c5b81aa5dc97a66046e2f101e9db457d34b63ce4
jonathantwite <13119611+jonathantwite@users.noreply.github.com>
2024-11-25 17:48:11 +0000

    Started setting form

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SimulatorService } from '../../services/simulator.service';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
+  private formBuilder = inject(FormBuilder);
+  private simulator = inject(SimulatorService);
 
+  showAdvancedSettings = signal(true);
+
+  settingsForm = this.formBuilder.group({
+    openingTime: ['', Validators.required],
+    closingTime: ['', Validators.required],
+    maxQueueRegisterDesk: [this.simulator.options().MaxQueueBallotBox],
+    numberOfRegisterDesks: [this.simulator.options().NumberOfRegisterDesks],
+    maxQueueVotingBooth: [this.simulator.options().MaxQueueVotingBooth],
+    numberOfVotingBooths: [this.simulator.options().NumberOfVotingBooths],
+    maxQueueBallotBox: [this.simulator.options().MaxQueueBallotBox],
+    numberOfBallotBoxes: [this.simulator.options().NumberOfBallotBoxes],
+    maxInBuilding: [this.simulator.options().MaxInBuilding],
+    minTimeInRegisterDeskQueue: [this.simulator.options().MinTimeInRegisterDeskQueue],
+    avgTimeAtRegisterDesk: [this.simulator.options().AvgTimeAtRegisterDesk],
+    minTimeInVotingBoothQueue: [this.simulator.options().MinTimeInVotingBoothQueue],
+    avgTimeAtVotingBooth: [this.simulator.options().AvgTimeAtVotingBooth],
+    minTimeInBallotBoxQueue: [this.simulator.options().MinTimeInBallotBoxQueue],
+    avgTimeAtBallotBox: [this.simulator.options().AvgTimeAtBallotBox],
+    minTimeInExitQueue: [this.simulator.options().MinTimeInExitQueue]
+  })
 }
