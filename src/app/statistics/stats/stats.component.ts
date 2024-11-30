206bfb0046c3a3770d057a912f02679c412e71ec
jonathantwite <13119611+jonathantwite@users.noreply.github.com>
2024-11-30 23:26:00 +0000

    Graphs WIP

--- src/app/statistics/stats/stats.component.ts
+++ src/app/statistics/stats/stats.component.ts
@@ -1,14 +1,46 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { StatsService } from '../services/stats.service';
+import { ApexAxisChartSeries, ChartComponent, NgApexchartsModule } from 'ng-apexcharts';
+
+type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+};
 
 @Component({
     selector: 'app-stats',
-    imports: [],
+    imports: [NgApexchartsModule],
     templateUrl: './stats.component.html',
     styleUrl: './stats.component.scss'
 })
 export class StatsComponent {
   private statsService = inject(StatsService);
+
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "My-series",
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "bar"
+      },
+      title: {
+        text: "My First Angular Chart"
+      },
+      xaxis: {
+        categories: ["Jan", "Feb",  "Mar",  "Apr",  "May",  "Jun",  "Jul",  "Aug", "Sep"]
+      }
+    };
+  }
 
   averageTotalSecondsUntilVoted = this.statsService.averageTotalSecondsUntilVoted;
 }
