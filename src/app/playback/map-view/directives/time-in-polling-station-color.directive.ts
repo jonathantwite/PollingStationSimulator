eae139d6fbc2d4fca9075165d3340c0d938a7671
jonathantwite <13119611+jonathantwite@users.noreply.github.com>
2025-01-14 08:54:53 +0000

    Added tooltip for length of time since arriving

--- src/app/playback/map-view/directives/time-in-polling-station-color.directive.ts
+++ src/app/playback/map-view/directives/time-in-polling-station-color.directive.ts
@@ -19,7 +19,7 @@ export class TimeInPollingStationColorDirective implements OnChanges {
     let timeInMinutes = this.currentTime()!.diff(this.timeArrived() as Time, 'Minutes');
     
     //console.log(this.timeArrived(), this.currentTime(), timeInMinutes);
-    
+    (this.el.nativeElement as HTMLElement).title = `${timeInMinutes} mins`
     if(timeInMinutes > 60) {  this.el.nativeElement.style.backgroundColor = 'red'; }
     else if(timeInMinutes > 15) {  this.el.nativeElement.style.backgroundColor = 'orange'; }
     else if(timeInMinutes > 5) {  this.el.nativeElement.style.backgroundColor = 'yellow'; }
